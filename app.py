fa3b10547d400c749548b7a1730428b0c9f57b54
projetoaprendizicmunamar <lukasfsr@gmail.com>
2025-07-03 21:40:09 -0300

    Update app.py
    Lucas Cézar Mendes de Souza

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ perfis = {
     "trompete": {
         "senha": "Unamar2025",
         "instrumento": "Trompete",
-        "alunos": ["Josué de Souza Silveira Júnior"]
+        "alunos": ["Josué de Souza Silveira Júnior", Lucas Cézar Mendes de Souza]
     }
 }
 
